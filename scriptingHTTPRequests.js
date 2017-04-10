bf5a9b129e5ede5f89cabd3113b00593a6b51c90
Shawn Yates <yates.sawnp@gmail.com>
2017-04-10 19:35:27 +0000

    Outputting data in chunks

---
+++ scriptingHTTPRequests.js
@@ -0,0 +1,21 @@
+const https = require('https');
+
+function getAndPrintHTMLChunks() {
+
+  const requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+  }
+
+  https.get(requestOptions, (response) => {
+    response.setEncoding('utf8');
+    response.on('data', (data) => {
+      console.log(`Chunk of data received: ${data} \n`);
+    });
+    response.on('end', () => {
+      console.log(`Response stream complete.`);
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
